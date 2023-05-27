074e8fd1342aff83a9ced89ebf0f9b95240af52b
ak2ie <mail@ak2ie.net>
2023-05-27 08:29:00 +0000

    update fetch notion api

--- my-app/app/articles/[slug]/page.tsx
+++ my-app/app/articles/[slug]/page.tsx
@@ -2,23 +2,78 @@ import { fetchPageBlocks, fetchPageBySlug, notion } from "@/lib/notion";
 import { notFound } from "next/navigation";
 import { NotionRenderer } from "@notion-render/client";
 type Props = {
-	params: { slug: string }
+  params: { slug: string };
 };
 
-const Articles = async ({ params: { slug } }: Props) => {
-	// await new Promise((resolve) => setTimeout(resolve, 2000))
-	const article = await fetchPageBySlug(slug)
-
-	if (!article) { notFound(); }
-
-	const blocks = await fetchPageBlocks(article.id);
-	const renderer = new NotionRenderer({
-		client: notion
-	})
-
-	const html = await renderer.render(...blocks);
-
-	return <div dangerouslySetInnerHTML={{ __html: html }}></div>
+async function getPageBySlug(slug: string) {
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${process.env.NOTION_DATABASE_ID}/query`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.NOTION_SECRET!}`,
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28",
+      },
+      body: JSON.stringify({
+        filter: {
+          and: [
+            {
+              property: "Slug",
+              rich_text: {
+                equals: slug,
+              },
+            },
+          ],
+        },
+      }),
+      next: {
+        revalidate: 10,
+      },
+    }
+  );
+  return response.json();
 }
 
-export default Articles
+async function getPageBlocks(pageId: string) {
+  const response = await fetch(
+    `https://api.notion.com/v1/blocks/${pageId}/children`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NOTION_SECRET!}`,
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28",
+      },
+      next: {
+        revalidate: 30, // キャッシュする時間[秒]
+      },
+    }
+  );
+  return response.json();
+}
+
+const Articles = async ({ params: { slug } }: Props) => {
+  // await new Promise((resolve) => setTimeout(resolve, 2000))
+  //   const article = await fetchPageBySlug(slug);
+  const article = await getPageBySlug(slug);
+
+  if (!article["results"].length) {
+    notFound();
+  }
+
+  // const blocks = await fetchPageBlocks(article["results"][0].id);
+  const blocks = await getPageBlocks(article["results"][0].id);
+  const renderer = new NotionRenderer({
+    client: notion,
+  });
+
+  const html = await renderer.render(...blocks.results);
+
+  return (
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+    </div>
+  );
+};
+
+export default Articles;
